9e230b5dc0ec068ce7b404ca8e90bf864a15c12d
engone nze davy <engonenzedavy@yahoo.com>
2014-02-04 11:26:12 -0800

    add basic content

---
+++ server/app-server.js
@@ -0,0 +1,9 @@
+// Here is the basic server 
+var express = require('express'),
+	app = express(),
+	path = require('path');
+
+// Add middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(8000);
