3e8318e28e737ab0aacaf868065d72b3612ee123
engone nze davy <engonenzedavy@yahoo.com>
2014-02-09 20:43:07 -0800

    add configuration file for the server and mongoose module

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,13 @@
-config.js
+module.exports = function(env){
+	var development = {
+		db : 'mongodb://localhost/sporten',
+		port: process.env.PORT || 5000
+	};
+
+	var production = {
+		db : '',
+		port: process.env.PORT || 5000
+	};
+
+	return env === 'development' ? development: production;
+}
